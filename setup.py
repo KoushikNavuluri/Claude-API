1b1a30d821e33207a8bcf1034789d6e8b653e8cc
Koushik Navuluri <103725723+KoushikNavuluri@users.noreply.github.com>
2023-07-17 10:58:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='claude-api', 
+    version='1.0.0',  
+    author='Koushik',
+    author_email='navulurikoushik@outlook.com',
+    description='An unofficial API for Claude AI, allowing users to access and interact with Claude AII',
+    long_description='This project provides an unofficial API for Claude AI, allowing users to access and interact with Claude A',
+    long_description_content_type='text/markdown',
+    url='https://github.com/KoushikNavuluri/Claude-API/', 
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    keywords=['claude', 'ai', 'claude-ai', 'API', 'requests', 'chatbot'],,
+    install_requires=[
+        'requests'  
+    ],
+    python_requires=">=3.7",
+)
