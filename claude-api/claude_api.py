dc2a67d3790bf3b1b6327102b653c875e2741c5f
Koushik Navuluri <103725723+KoushikNavuluri@users.noreply.github.com>
2023-07-22 15:28:18 +0530

    Update claude_api.py

--- claude-api/claude_api.py
+++ claude-api/claude_api.py
@@ -72,49 +72,49 @@ class Client:
       print(f"Error: {response.status_code} - {response.text}")
 
   # Send Message to Claude
- def send_message(self, prompt, conversation_id, attachment=None):
+  def send_message(self, prompt, conversation_id, attachment=None):
     url = "https://claude.ai/api/append_message"
 
     # Upload attachment if provided
     attachments = []
     if attachment:
-        attachment_response = self.upload_attachment(attachment)
-        if attachment_response:
-            attachments = attachment_response
-        else:
-            return {"Error: Invalid file format. Please try again."}
-    
+      attachment_response = self.upload_attachment(attachment)
+      if attachment_response:
+        attachments = attachment_response
+      else:
+        return {"Error: Invalid file format. Please try again."}
+
     # Ensure attachments is an empty list when no attachment is provided
     if not attachment:
-        attachments = []
+      attachments = []
 
     payload = json.dumps({
-        "completion": {
-            "prompt": f"{prompt}",
-            "timezone": "Asia/Kolkata",
-            "model": "claude-2"
-        },
-        "organization_uuid": f"{self.organization_id}",
-        "conversation_uuid": f"{conversation_id}",
-        "text": f"{prompt}",
-        "attachments": attachments
+      "completion": {
+        "prompt": f"{prompt}",
+        "timezone": "Asia/Kolkata",
+        "model": "claude-2"
+      },
+      "organization_uuid": f"{self.organization_id}",
+      "conversation_uuid": f"{conversation_id}",
+      "text": f"{prompt}",
+      "attachments": attachments
     })
 
     headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-        'Accept': 'text/event-stream, text/event-stream',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Referer': 'https://claude.ai/chats',
-        'Content-Type': 'application/json',
-        'Origin': 'https://claude.ai',
-        'DNT': '1',
-        'Connection': 'keep-alive',
-        'Cookie': f'{self.cookie}',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'TE': 'trailers'
+      'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+      'Accept': 'text/event-stream, text/event-stream',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Referer': 'https://claude.ai/chats',
+      'Content-Type': 'application/json',
+      'Origin': 'https://claude.ai',
+      'DNT': '1',
+      'Connection': 'keep-alive',
+      'Cookie': f'{self.cookie}',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-origin',
+      'TE': 'trailers'
     }
 
     response = requests.post(url, headers=headers, data=payload, stream=True)
