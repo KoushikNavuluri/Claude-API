2767cb466297c0ba7c51d8f5be96c70216087cbc
Koushik Navuluri <103725723+KoushikNavuluri@users.noreply.github.com>
2023-07-22 06:50:25 +0530

    Update claude_api.py

--- claude-api/claude_api.py
+++ claude-api/claude_api.py
@@ -12,41 +12,54 @@ class Client:
 
   def get_organization_id(self):
     url = "https://claude.ai/api/organizations"
-  
+
     headers = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-      'Accept': '*/*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Referer': 'https://claude.ai/chats',
-      'Content-Type': 'application/json',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'Connection': 'keep-alive',
-      'Cookie': f'{self.cookie}'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://claude.ai/chats',
+        'Content-Type': 'application/json',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Connection': 'keep-alive',
+        'Cookie': f'{self.cookie}'
     }
-  
+
     response = requests.request("GET", url, headers=headers)
     res = json.loads(response.text)
     uuid = res[0]['uuid']
-  
+
     return uuid
+
+  def get_content_type(self, file_path):
+    # Function to determine content type based on file extension
+    extension = os.path.splitext(file_path)[-1].lower()
+    if extension == '.pdf':
+      return 'application/pdf'
+    elif extension == '.txt':
+      return 'text/plain'
+    elif extension == '.csv':
+      return 'text/csv'
+    # Add more content types as needed for other file types
+    else:
+      return 'application/octet-stream'
 
   # Lists all the conversations you had with Claude
   def list_all_conversations(self):
     url = f"https://claude.ai/api/organizations/{self.organization_id}/chat_conversations"
 
     headers = {
-      'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Referer': 'https://claude.ai/chats',
-      'Content-Type': 'application/json',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'Connection': 'keep-alive',
-      'Cookie': f'{self.cookie}'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://claude.ai/chats',
+        'Content-Type': 'application/json',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Connection': 'keep-alive',
+        'Cookie': f'{self.cookie}'
     }
 
     response = requests.get(url, headers=headers)
@@ -59,35 +72,46 @@ class Client:
       print(f"Error: {response.status_code} - {response.text}")
 
   # Send Message to Claude
-  def send_message(self, prompt, conversation_id):
+  def send_message(self,  prompt, conversation_id,attachment=None):
     url = "https://claude.ai/api/append_message"
 
+    # Upload attachment if provided
+    attachments = []
+    if attachment:
+      attachment_response = self.upload_attachment(attachment)
+      if attachment_response:
+        attachments = attachment_response
+      else:
+        return {"Error: Invalid file format .Please try again."}
+        
+
     payload = json.dumps({
-      "completion": {
-        "prompt": f"{prompt}",
-        "timezone": "Asia/Kolkata",
-        "model": "claude-2"
-      },
-      "organization_uuid": f"{self.organization_id}",
-      "conversation_uuid": f"{conversation_id}",
-      "text": f"{prompt}",
-      "attachments": []
+        "completion": {
+            "prompt": f"{prompt}",
+            "timezone": "Asia/Kolkata",
+            "model": "claude-2"
+        },
+        "organization_uuid": f"{self.organization_id}",
+        "conversation_uuid": f"{conversation_id}",
+        "text": f"{prompt}",
+        "attachments": [attachments]
     })
+
     headers = {
-      'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-      'Accept': 'text/event-stream, text/event-stream',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Referer': 'https://claude.ai/chats',
-      'Content-Type': 'application/json',
-      'Origin': 'https://claude.ai',
-      'DNT': '1',
-      'Connection': 'keep-alive',
-      'Cookie': f'{self.cookie}',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'TE': 'trailers'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept': 'text/event-stream, text/event-stream',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://claude.ai/chats',
+        'Content-Type': 'application/json',
+        'Origin': 'https://claude.ai',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Cookie': f'{self.cookie}',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'TE': 'trailers'
     }
 
     response = requests.post(url, headers=headers, data=payload, stream=True)
@@ -105,20 +129,19 @@ class Client:
 
     payload = json.dumps(f"{conversation_id}")
     headers = {
-      'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-      'Accept': '*/*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Content-Type': 'application/json',
-      'Content-Length': '38',
-      'Referer': 'https://claude.ai/chats',
-      'Origin': 'https://claude.ai',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'Connection': 'keep-alive',
-      'Cookie': f'{self.cookie}',
-      'TE': 'trailers'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/json',
+        'Content-Length': '38',
+        'Referer': 'https://claude.ai/chats',
+        'Origin': 'https://claude.ai',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Connection': 'keep-alive',
+        'Cookie': f'{self.cookie}',
+        'TE': 'trailers'
     }
 
     response = requests.request("DELETE", url, headers=headers, data=payload)
@@ -134,16 +157,16 @@ class Client:
     url = f"https://claude.ai/api/organizations/{self.organization_id}/chat_conversations/{conversation_id}"
 
     headers = {
-      'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Referer': 'https://claude.ai/chats',
-      'Content-Type': 'application/json',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'Connection': 'keep-alive',
-      'Cookie': f'{self.cookie}'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://claude.ai/chats',
+        'Content-Type': 'application/json',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Connection': 'keep-alive',
+        'Cookie': f'{self.cookie}'
     }
 
     response = requests.request("GET", url, headers=headers)
@@ -158,27 +181,25 @@ class Client:
     formatted_uuid = f"{random_uuid_str[0:8]}-{random_uuid_str[9:13]}-{random_uuid_str[14:18]}-{random_uuid_str[19:23]}-{random_uuid_str[24:]}"
     return formatted_uuid
 
-  
   def create_new_chat(self):
     url = f"https://claude.ai/api/organizations/{self.organization_id}/chat_conversations"
     uuid = self.generate_uuid()
 
     payload = json.dumps({"uuid": uuid, "name": ""})
     headers = {
-      'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-      'Accept': '*/*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Referer': 'https://claude.ai/chats',
-      'Content-Type': 'application/json',
-      'Origin': 'https://claude.ai',
-      'DNT': '1',
-      'Connection': 'keep-alive',
-      'Cookie': self.cookie,
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'TE': 'trailers'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://claude.ai/chats',
+        'Content-Type': 'application/json',
+        'Origin': 'https://claude.ai',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Cookie': self.cookie,
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'TE': 'trailers'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
@@ -196,28 +217,60 @@ class Client:
 
     return True
 
+  def upload_attachment(self, file_path):
+    url = 'https://claude.ai/api/convert_document'
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://claude.ai/chats',
+        'Origin': 'https://claude.ai',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Connection': 'keep-alive',
+        'Cookie': f'{self.cookie}',
+        'TE': 'trailers'
+    }
+
+    file_name = os.path.basename(file_path)
+    content_type = self.get_content_type(file_path)
+
+    files = {
+        'file': (file_name, open(file_path, 'rb'), content_type),
+        'orgUuid': (None, self.organization_id)
+    }
+
+    response = requests.post(url, headers=headers, files=files)
+    if response.status_code == 200:
+      return response.json()
+    else:
+      return False
+      
+
+    
   # Renames the chat conversation title
   def rename_chat(self, title, conversation_id):
     url = "https://claude.ai/api/rename_chat"
 
     payload = json.dumps({
-      "organization_uuid": f"{self.organization_id}",
-      "conversation_uuid": f"{conversation_id}",
-      "title": f"{title}"
+        "organization_uuid": f"{self.organization_id}",
+        "conversation_uuid": f"{conversation_id}",
+        "title": f"{title}"
     })
     headers = {
-      'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Content-Type': 'application/json',
-      'Referer': 'https://claude.ai/chats',
-      'Origin': 'https://claude.ai',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'Connection': 'keep-alive',
-      'Cookie': f'{self.cookie}',
-      'TE': 'trailers'
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/json',
+        'Referer': 'https://claude.ai/chats',
+        'Origin': 'https://claude.ai',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Connection': 'keep-alive',
+        'Cookie': f'{self.cookie}',
+        'TE': 'trailers'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
@@ -226,3 +279,4 @@ class Client:
       return True
     else:
       return False
+      
