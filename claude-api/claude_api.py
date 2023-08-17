184f52cb57f31882fb3e529a0a876b1d6b5bf363
Koushik Navuluri <103725723+KoushikNavuluri@users.noreply.github.com>
2023-08-17 11:24:32 +0530

    Update claude_api.py

--- claude-api/claude_api.py
+++ claude-api/claude_api.py
@@ -1,7 +1,7 @@
-import requests
 import json
 import os
 import uuid
+from curl_cffi import requests
 import re
 
 
@@ -27,7 +27,7 @@ class Client:
         'Cookie': f'{self.cookie}'
     }
 
-    response = requests.request("GET", url, headers=headers)
+    response = requests.get(url, headers=headers,impersonate="chrome110")
     res = json.loads(response.text)
     uuid = res[0]['uuid']
 
@@ -63,7 +63,7 @@ class Client:
         'Cookie': f'{self.cookie}'
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers,impersonate="chrome110")
     conversations = response.json()
 
     # Returns all conversation information in a list
@@ -118,7 +118,7 @@ class Client:
       'TE': 'trailers'
     }
 
-    response = requests.post(url, headers=headers, data=payload, stream=True)
+    response = requests.post( url, headers=headers, data=payload,impersonate="chrome110")
     decoded_data = response.content.decode("utf-8")
     decoded_data = re.sub('\n+', '\n', decoded_data).strip()
     data_strings = decoded_data.split('\n')
@@ -155,7 +155,7 @@ class Client:
         'TE': 'trailers'
     }
 
-    response = requests.request("DELETE", url, headers=headers, data=payload)
+    response = requests.delete( url, headers=headers, data=payload,impersonate="chrome110")
 
     # Returns True if deleted or False if any error in deleting
     if response.status_code == 204:
@@ -180,8 +180,8 @@ class Client:
         'Cookie': f'{self.cookie}'
     }
 
-    response = requests.request("GET", url, headers=headers)
-    print(type(response))
+    response = requests.get( url, headers=headers,impersonate="chrome110")
+    
 
     # List all the conversations in JSON
     return response.json()
@@ -213,7 +213,7 @@ class Client:
         'TE': 'trailers'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.post( url, headers=headers, data=payload,impersonate="chrome110")
 
     # Returns JSON of the newly created conversation information
     return response.json()
@@ -265,7 +265,7 @@ class Client:
         'orgUuid': (None, self.organization_id)
     }
 
-    response = requests.post(url, headers=headers, files=files)
+    response = requests.post(url, headers=headers, files=files,impersonate="chrome110")
     if response.status_code == 200:
       return response.json()
     else:
@@ -297,7 +297,7 @@ class Client:
         'TE': 'trailers'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.post(url, headers=headers, data=payload,impersonate="chrome110")
 
     if response.status_code == 200:
       return True
