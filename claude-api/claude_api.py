e8ab85375ef0c61423fe3de9d921aee1b5d14d08
艾梦 <HighCWu@163.com>
2023-08-28 09:17:29 +0800

    Add retry_message api

--- claude-api/claude_api.py
+++ claude-api/claude_api.py
@@ -74,7 +74,7 @@ class Client:
       print(f"Error: {response.status_code} - {response.text}")
 
   # Send Message to Claude
-  def send_message(self, prompt, conversation_id, attachment=None,timeout=500):
+  def send_message(self, prompt, conversation_id, attachment=None, timeout=500):
     url = "https://claude.ai/api/append_message"
 
     # Upload attachment if provided
@@ -119,7 +119,7 @@ class Client:
       'TE': 'trailers'
     }
 
-    response = requests.post( url, headers=headers, data=payload,impersonate="chrome110",timeout=500)
+    response = requests.post( url, headers=headers, data=payload, impersonate="chrome110", timeout=timeout)
     decoded_data = response.content.decode("utf-8")
     decoded_data = re.sub('\n+', '\n', decoded_data).strip()
     data_strings = decoded_data.split('\n')
@@ -135,6 +135,54 @@ class Client:
     # Returns answer
     return answer
 
+  # Regenerate Message from Claude
+  def retry_message(self, conversation_id, timeout=500):
+    url = "https://claude.ai/api/retry_message"
+
+    payload = json.dumps({
+      "completion": {
+        "prompt": "",
+        "timezone": "Asia/Kolkata",
+        "model": "claude-2"
+      },
+      "organization_uuid": f"{self.organization_id}",
+      "conversation_uuid": f"{conversation_id}",
+      "text": ""
+    })
+
+    headers = {
+      'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+      'Accept': 'text/event-stream, text/event-stream',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Referer': 'https://claude.ai/chats',
+      'Content-Type': 'application/json',
+      'Origin': 'https://claude.ai',
+      'DNT': '1',
+      'Connection': 'keep-alive',
+      'Cookie': f'{self.cookie}',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-origin',
+      'TE': 'trailers'
+    }
+
+    response = requests.post( url, headers=headers, data=payload, impersonate="chrome110", timeout=timeout)
+    decoded_data = response.content.decode("utf-8")
+    decoded_data = re.sub('\n+', '\n', decoded_data).strip()
+    data_strings = decoded_data.split('\n')
+    completions = []
+    for data_string in data_strings:
+      json_str = data_string[6:].strip()
+      data = json.loads(json_str)
+      if 'completion' in data:
+        completions.append(data['completion'])
+
+    answer = ''.join(completions)
+
+    # Returns answer
+    return answer
+  
   # Deletes the conversation
   def delete_conversation(self, conversation_id):
     url = f"https://claude.ai/api/organizations/{self.organization_id}/chat_conversations/{conversation_id}"
